56c991c61e2a4ba9a95624f79272225237ec7ae5
Ishwor <ishwo5678@gmail.com>
2024-04-23 19:06:35 +1000

    using jasmine for money test

---
+++ Amazon-Clone-JS-project--/tests-jasmine/moneyTest.js
@@ -0,0 +1,18 @@
+import { currency } from "../scripts/utils/money.js";
+
+//console.log('test suite: currency')
+// if (currency(20095)==='20.95') {
+//     console.log('passes')
+//    } else{
+//        console.log('failedd')
+//    }
+
+describe('test suite: currency',()=>{
+    it('converts cents into dollar ',()=>{
+        expect(currency(2095)).toEqual('20.95');
+        })
+    
+        it('works with 0',()=>{
+            expect(currency(0)).toEqual('0.00');
+        })
+})

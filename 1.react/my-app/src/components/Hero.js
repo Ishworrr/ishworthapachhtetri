13d064fcac320cb333fd729c149b85a768511141
Ishwor <ishwo5678@gmail.com>
2024-05-24 14:42:15 +1000

    all card business

---
+++ 1.react/my-app/src/components/Hero.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function Hero(){
+    return(
+        <section className='hero'>
+            <img src='./Vector.png' className='hero--photo'/>
+                <h1 className='hero--header'>Hero</h1>
+                <p className='hero--text'>qqx</p>
+        </section>
+    )
+}

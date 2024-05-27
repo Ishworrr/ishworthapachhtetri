4d12fcc9f5b965bb469ebccb663ad74a1ebb8e40
Ishwor <ishwo5678@gmail.com>
2024-05-27 18:12:53 +1000

    start1 end and start2 start

--- Scrim-s0tei5l9fc/new/Joke.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export default function Joke(Setup, Punchline){
-return (
-    <h1>Joke baby</h1>
-)
-
-
-}
--- Scrim-s0tei5l9fc/new/indexs.js
+++
@@ -1,5 +0,0 @@
-import React from "react"
-import ReactDOM from "react-dom"
-import Apps from "./Apps"
-
-ReactDOM.render(<Apps />, document.getElementById("root"))

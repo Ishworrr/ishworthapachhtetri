5c4b110c0c2f6c4b0310e8d01b29c20651818117
Ishwor <ishwo5678@gmail.com>
2024-05-30 22:01:07 +1000

    react playlist 1.o

---
+++ 3.reract-10-proj/project1/src/components/Navigation.jsx
@@ -0,0 +1,18 @@
+const Navigation =()=>{
+    return(
+        <nav>  
+   <div className="logo">
+     <img src="../images/nike.png"/>
+     </div>
+    <ul>
+      <li><a href="/">Home</a></li>
+      <li href="#">Menu</li>
+    </ul>
+
+    <button>login</button>
+    </nav>
+
+    )
+}
+
+export default Navigation;
